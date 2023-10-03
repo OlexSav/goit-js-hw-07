9d3513ccf25125f99b045af4561643362ebdfd4b
Olexandr Savchenko <a.savchenko1207@gmail.com>
2023-10-03 23:05:13 +0300

    first task JS

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,49 @@
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
+const pictureContainer = document.querySelector("ul.gallery");
+const cardMarkup = createPictureCards(galleryItems);
+
+pictureContainer.insertAdjacentHTML("beforeend", cardMarkup);
+pictureContainer.addEventListener("click", choosePictureCard);
+
+function createPictureCards(item) {
+  return item
+    .map(
+      ({ preview, original, description }) => `
+    <li class="gallery__item">
+      <a class="gallery__link" href="${original}">
+        <img
+          class="gallery__image"
+          src="${preview}"
+          data-source="${original}"
+          alt="${description}"
+        />
+      </a>
+    </li>
+  `
+    )
+    .join("");
+}
+
+let lightBox;
+
+function choosePictureCard(evt) {
+  evt.preventDefault();
+  if (evt.target.tagName === "IMG") {
+    lightBox = basicLightbox.create(
+      `<img src="${evt.target.dataset.source}" alt="${evt.target.alt}">`
+    );
+    lightBox.show();
+  }
+  window.addEventListener("keydown", closeModalWindow);
+}
+
+function closeModalWindow(evt) {
+  if (evt.key === "Escape") {
+    lightBox.close();
+    window.removeEventListener("keydown", closeModalWindow);
+  }
+}
+
 console.log(galleryItems);
